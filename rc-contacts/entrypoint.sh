efe4fb769d3ca395de40f498eaf731539145fe85
Rafael Ortiz <rd.ortizr1@uniandes.edu.co>
2023-07-26 17:40:08 -0500

    [Refactor] - Update docker files

---
+++ rc-contacts/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Perform any pre-migration setup here (if needed)
+
+# Run Laravel migrations
+php artisan migrate
+
+# Start the PHP development server
+php artisan serve --host=0.0.0.0 --port=8000
+
+# Add any post-migration commands here (if needed)
